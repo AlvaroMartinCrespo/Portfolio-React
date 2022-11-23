32d2151fdf7602b3369fe341795bf067b8643311
Alvaro <alvaro.martin.crespo.00@gmail.com>
2022-11-23 10:15:23 +0100

    nav logo favicon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import '../stylesheet/Nav.css';
+import imgLogo from '../img/utilities/code2.png';
+
+const code1 = '<';
+const code2 = '>';
+const name = 'AMC';
 
 function Nav() {
     return (
         <div className="mainContainer">
             <div className="containerName">
-                <span className="blue">[</span>
-                <span>dev-AMC</span>
-                <span className="blue">]</span>
+                <span className="blueSpan">{code1}</span>
+                <span>{name}</span>
+                <span className="greenSpan">{code2}</span>
             </div>
             <div className="containerList">
                 <nav>
                     <ul>
-                        <li>Cosa 1</li>
-                        <li>Cosa 2</li>
-                        <li>Cosa 3</li>
+                        <li>Knowledge</li>
+                        <li>Portfolio</li>
+                        <li>Contact</li>
                     </ul>
                 </nav>
             </div>
