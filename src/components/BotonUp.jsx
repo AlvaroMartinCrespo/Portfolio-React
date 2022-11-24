41aa751c98f61a29012f3db8e0273acb9752acdc
Alvaro <alvaro.martin.crespo.00@gmail.com>
2022-11-24 13:02:09 +0100

    update

--- src/components/BotonUp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Arrow from '../img/utilities/arrowUp.png';
-
-function BotonUp() {
-    const url = '#';
-    return <div className=""></div>;
-}
-
-export default BotonUp;
