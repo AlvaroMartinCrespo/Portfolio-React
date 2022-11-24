2eb8d56b2bc4d347d6e962acee198653a0baf836
Alvaro <alvaro.martin.crespo.00@gmail.com>
2022-11-24 13:23:36 +0100

    update

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import VideoBg from '../video/video1.mp4';
+import VideoBg2 from '../video/video2.mp4';
 import '../stylesheet/Video.css';
 
 function Video() {
     return (
         <div className="containerVideoBg">
-            <video src={VideoBg} autoPlay loop muted />
+            <video src={VideoBg2} autoPlay loop muted />
         </div>
     );
 }
