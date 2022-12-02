55e422bb2ca37056939f7d2dc6411604430b2d44
Alvaro <alvaro.martin.crespo.00@gmail.com>
2022-12-02 12:23:44 +0100

    update

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -3,22 +3,18 @@ import '../stylesheet/Presentation.css';
 import imgPortfolio from '../img/imgState/img3.jpg';
 
 function Presentation({ name, description, skills }) {
-    return (
-        <div className="contenedor-testimonio">
-            <img
-                className="imagen-testimonio"
-                src={imgPortfolio}
-                alt="portfio-img"
-            />
-            <div className="contenedor-texto">
-                <p className="nombre-testimonio">
-                    <strong>{name}</strong>
-                </p>
-                <p className="texto-testimonio">"{description}"</p>
-                <p className="texto-testimonio">"{skills}"</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="contenedor-testimonio">
+      <img className="imagen-testimonio" src={imgPortfolio} alt="portfio-img" />
+      <div className="contenedor-texto">
+        <p className="nombre-testimonio">
+          <strong className="transparent">{name}</strong>
+        </p>
+        <p className="texto-testimonio">"{description}"</p>
+        <p className="texto-testimonio">"{skills}"</p>
+      </div>
+    </div>
+  );
 }
 
 export default Presentation;
